d7133e748ecf5d9e8fff7ed86c3e7cdbb8340cb6
Jack Givens <>
2024-11-25 20:13:33 -0600

    add react-hooks linter plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,7 @@ import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
 import eslintConfigPrettier from "eslint-plugin-prettier/recommended";
+import pluginHooks from 'eslint-plugin-react-hooks';
 
 
 /** @type {import('eslint').Linter.Config[]} */
@@ -13,6 +14,21 @@ export default [
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
   pluginReact.configs.flat['jsx-runtime'],
+  //taken from https://github.com/facebook/react/pull/30774. TODO: when this PR is published,
+  //replace this with pluginHooks.configs['recommended-latest']
+  {
+    name: 'react-hooks/recommended',
+    plugins: {
+      'react-hooks': {
+        meta: {
+          name: 'eslint-plugin-react-hooks',
+          version: '5.0.0'
+        },
+        rules: pluginHooks.rules
+      }
+    },
+    rules: pluginHooks.configs.recommended.rules,
+  },
   {
     ignores: ['node_modules', 'public', 'dist'],
   },
